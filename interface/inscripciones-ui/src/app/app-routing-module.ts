4dcc35ba64bc1f5efb29b5629675a3658f559946
diegovr007 <direve777@gmail.com>
2026-06-27 19:30:56 -0600

    Routes created with nav and css styles

--- interface/inscripciones-ui/src/app/app-routing-module.ts
+++ interface/inscripciones-ui/src/app/app-routing-module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'resumen',
+    pathMatch: 'full'
+  },
+  {
+    path: 'resumen',
+    loadChildren: () =>
+      import('./modules/resumen/resumen-module').then(m => m.ResumenModule)
+  },
+  {
+    path: 'alumnos',
+    loadChildren: () =>
+      import('./modules/alumnos/alumnos-module').then(m => m.AlumnosModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () =>
+      import('./modules/registro/registro-module').then(m => m.RegistroModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'resumen'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
